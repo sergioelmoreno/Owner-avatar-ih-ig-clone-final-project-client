f65e1eb25094e6d2f40dae06e2321a1838b41c05
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-08-29 14:55:19 +0200

    fix navigation style

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -35,7 +35,8 @@ const Navigation = () => {
             <strong>Aperture</strong>
           </Stack>
         </Navbar.Brand>
-        <div className="d-flex flex-column flex-md-row justify-content-end align-items-center">
+
+        <div className="d-flex ">
           <div className="d-flex align-items-center">
 
             {
@@ -53,50 +54,50 @@ const Navigation = () => {
           <Button variant="link" onClick={handleShow} className="ms-2">
             <img src={list} alt="Menu" />
           </Button>
-          <Offcanvas show={show} onHide={handleClose} placement={'end'} name={'end'}>
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title>Menu</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Stack gap={2} className='h-100'>
-                <div>
-
-                  {
-                    loggedUser
-                      ?
-                      <>
-                        <Nav className="me-3">
-                          <Nav.Link as={Link} to={'/posts/new'}>New Post</Nav.Link>
-                        </Nav>
-                        <Nav className="me-3">
-                          <Nav.Link as={Link} onClick={handleLogout}>Logout</Nav.Link>
-                        </Nav>
-                      </>
-                      :
-                      <>
-                        <Nav className="me-3">
-                          <Nav.Link as={Link} to={'/signup'}>Signup</Nav.Link>
-                        </Nav>
-                        <Nav>
-                          <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
-                        </Nav>
-                      </>
-                  }
-                </div>
-
-                <div className="mt-auto">
-                  <h4>About me:</h4>
-                  <Stack gap={2} direction='horizontal'>
-                    <Image src={sergioelmoreno} roundedCircle className='thumb-user-avatar' />
-                    <a href="https://github.com/sergioelmoreno" target='blank' className='external-link'>Sergioelmoreno</a>
-                  </Stack>
-
-                </div>
-              </Stack>
-            </Offcanvas.Body>
-          </Offcanvas>
-
         </div>
+        <Offcanvas show={show} onHide={handleClose} placement={'end'} name={'end'}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Stack gap={2} className='h-100'>
+              <div>
+
+                {
+                  loggedUser
+                    ?
+                    <>
+                      <Nav className="me-3">
+                        <Nav.Link as={Link} to={'/posts/new'}>New Post</Nav.Link>
+                      </Nav>
+                      <Nav className="me-3">
+                        <Nav.Link as={Link} onClick={handleLogout}>Logout</Nav.Link>
+                      </Nav>
+                    </>
+                    :
+                    <>
+                      <Nav className="me-3">
+                        <Nav.Link as={Link} to={'/signup'}>Signup</Nav.Link>
+                      </Nav>
+                      <Nav>
+                        <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
+                      </Nav>
+                    </>
+                }
+              </div>
+
+              <div className="mt-auto">
+                <h4>About me:</h4>
+                <Stack gap={2} direction='horizontal'>
+                  <Image src={sergioelmoreno} roundedCircle className='thumb-user-avatar' />
+                  <a href="https://github.com/sergioelmoreno" target='blank' className='external-link'>Sergioelmoreno</a>
+                </Stack>
+
+              </div>
+            </Stack>
+          </Offcanvas.Body>
+        </Offcanvas>
+
 
       </Container>
     </Navbar>
